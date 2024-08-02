7b5ac666ec356a159fe6f889fc796f04144d9d23
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-02 16:41:40 -0300

    add dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv'; 
 import { syncDatabase } from './shared/infrastructure/db/db.sequelize.config';
 
 async function bootstrap() {
-  dotenv.config()
+  dotenv.config();
+  console.log(process.env.DATABASE)
+  console.log(process.env.PORT)
   const app = await NestFactory.create(AppModule);
   syncDatabase();
   await app.listen(3000);
