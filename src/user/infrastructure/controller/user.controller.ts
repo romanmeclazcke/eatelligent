e00a9af35b6d58dd30373054548c191dc544eac5
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-05 19:33:22 -0300

    create user controller

--- src/user/infrastructure/controller/user.controller.ts
+++ src/user/infrastructure/controller/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req, Res} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, Res} from '@nestjs/common';
 import { Request,Response } from 'express';
 import { userUseCases } from 'src/user/application/user.use.cases';
+import { userCreate } from 'src/user/domian/dto/user.create';
 
 
 
@@ -10,7 +11,7 @@ export class userController {
     constructor(private userUseCase:userUseCases){}
 
     @Get()
-    async getAll(@Req() request:Request,@Res() response:Response) {
+    async getUsers(@Req() request:Request,@Res() response:Response) {
          const result = await  this.userUseCase.getAllUser();
          if(result.isSucces){
             response.status(200).json({message:result.value})
@@ -18,5 +19,25 @@ export class userController {
             response.status(404).json({message:result.error})
          }
     }
-    
-}  
+
+    @Get(':id')
+    async getUserById(@Param('id') id:string, @Req() request:Request, @Res() response:Response){
+      const result = await this.userUseCase.getUserById(id);
+      if(result.isSucces){
+         response.status(result.statusCode).json({message:result.value,details:true})
+      }else{
+         response.status(result.statusCode).json({message:result.error,details:true})
+      }
+    }
+
+    @Post("/new")
+    async createUser(@Body() createUserDto: userCreate,@Req() request:Request,@Res() response:Response){
+      const result = await this.userUseCase.createUser(createUserDto);
+      if(result.isSucces){
+         response.status(result.statusCode).json({message:result.value,details:true})
+      }else{
+         response.status(result.statusCode).json({message:result.error,details:true})
+      }
+    }
+
+   }  
