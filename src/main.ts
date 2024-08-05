54e34107d29abb03d1404552b325533ee6a504a0
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-05 19:36:43 -0300

    remove console.log

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { syncDatabase } from './shared/infrastructure/db/db.sequelize.config';
 
 async function bootstrap() {
   dotenv.config();
-  console.log(process.env.DATABASE)
-  console.log(process.env.PORT)
   const app = await NestFactory.create(AppModule);
   syncDatabase();
   await app.listen(3000);
