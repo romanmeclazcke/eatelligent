1710fdd61fe33911ec6fb2d01bb5b15fe59eb5a2
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-05 19:31:19 -0300

    create method to user user case

--- src/user/application/user.use.cases.ts
+++ src/user/application/user.use.cases.ts
@@ -2,18 +2,51 @@ import { Injectable } from "@nestjs/common";
 import { userRepositorySequelize } from "../infrastructure/repository/user.repository.sequelize";
 import { userEntity } from "../domian/user.entity";
 import { Result } from "src/shared/infrastructure/patternResult/result";
+import { userCreate } from "../domian/dto/user.create";
+import { create } from "domain";
 
 @Injectable()
 export class userUseCases {
     
     constructor(private userRepository: userRepositorySequelize){}
 
-    async getAllUser(): Promise<Result<userEntity[]>>{
+    async getAllUser(): Promise<Result<userEntity[]|null>>{
        const user = await this.userRepository.getUsers();
         if(user){
             return Result.succes(user,200)
         }else{
             return  Result.failure("user not found",404)
         }
+    }
+
+    async getUserById(id:string):Promise<Result<userEntity|null>>{
+        const user = await this.userRepository.getUserById(id);
+        if(user){
+            return Result.succes(user,200)
+        }else{
+            return Result.failure("user not found",404)
+        }
+    }
+
+    async createUser(createUser:userCreate):Promise<Result<userEntity|null>>{
+        const userCreated= await this.userRepository.getUserByEmail(createUser.email);
+        console.log(userCreated)
+
+        if(userCreated!){
+            return Result.failure("User with email already exists",404);
+        }
+        const usernameInUse= await this.userRepository.getUserByUserName(createUser.userName);
+
+        if(usernameInUse){
+            return Result.failure("User with username already exists",404);
+        }
+
+    
+        const user = await this.userRepository.createUser(createUser);
+        if(user){
+            return Result.succes(user,200)
+        }else{
+            return Result.failure("user not found",404)
+        }   
     }
 }
