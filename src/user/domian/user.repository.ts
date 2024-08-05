76961e07dfb40689c51b5ede73204ff07dd0b5ad
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-05 19:33:15 -0300

    add new method o getUser and create

--- src/user/domian/user.repository.ts
+++ src/user/domian/user.repository.ts
@@ -1,6 +1,11 @@
+import { userCreate } from "./dto/user.create";
 import { userEntity } from "./user.entity";
 
 export interface userRepository{
     
     getUsers():Promise<userEntity[]|null>
+    getUserById(id:string):Promise<userEntity|null>
+    createUser(user:userCreate):Promise<userEntity|null>
+    getUserByEmail(email: string):Promise<userEntity|null>
+    getUserByUserName(userName:string):Promise<userEntity|null>
 }
