203365ba9bb86dce640fdd1dc88ce37602a66ce5
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-06 13:16:45 -0300

    add global pipes

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'; 
 import { syncDatabase } from './shared/infrastructure/db/db.sequelize.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   syncDatabase();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidNonWhitelisted:true //no acepta peticiones con propiedaes que no necesita
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
