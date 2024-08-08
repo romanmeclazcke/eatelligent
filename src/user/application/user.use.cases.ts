969a7c59f4c0a044a2f161103173d37e33774341
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-08 19:43:45 -0300

    dependency injection about the module cloudinary

--- src/user/application/user.use.cases.ts
+++ src/user/application/user.use.cases.ts
@@ -3,12 +3,14 @@ import { userRepositorySequelize } from "../infrastructure/repository/user.repos
 import { UserEntity } from "../domian/user.entity";
 import { Result } from "src/shared/infrastructure/patternResult/result";
 import { CreateUserDto } from "../domian/dto/create.user.dto";
+import { CloudinaryService } from "src/shared/infrastructure/cloudinary/cloudinary.service";
+
 
 
 @Injectable()
 export class userUseCases {
     
-    constructor(private userRepository: userRepositorySequelize){}
+    constructor(private userRepository: userRepositorySequelize, private cloudinary: CloudinaryService){}
 
     async getAllUser(): Promise<Result<UserEntity[]|null>>{
        const user = await this.userRepository.getUsers();
@@ -28,9 +30,9 @@ export class userUseCases {
         }
     }
 
-    async createUser(createUser:CreateUserDto):Promise<Result<UserEntity|null>>{
+    async createUser(createUser:CreateUserDto,file: Express.Multer.File):Promise<Result<UserEntity|null>>{
         const emailInUse= await this.userRepository.getUserByEmail(createUser.email);
-        
+         
         if(emailInUse){
             return Result.failure("User with email already exists",404);
         }
@@ -40,11 +42,24 @@ export class userUseCases {
             return Result.failure("User with username already exists",404);
         }
 
-        const user = await this.userRepository.createUser(createUser);
+        let profilePictureUrl: string | null = null;
+
+        if (file) {
+            try {
+                const uploadResult = await this.cloudinary.uploadImage(file);
+                profilePictureUrl = uploadResult.url; 
+            } catch (uploadError) {
+                return Result.failure("Failed to upload image", 500);
+            }
+        }
+        const userWithImage: CreateUserDto = { ...createUser, profilePicture: profilePictureUrl }
+        
+        const user = await this.userRepository.createUser(userWithImage);
+
         if(user){
             return Result.succes(user,201)
         }else{
-            return Result.failure("user not found",404)
+            return Result.failure("Error to create user",500)
         }   
     }
 }
