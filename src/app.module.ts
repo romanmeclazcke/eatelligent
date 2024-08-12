936383d8733908b7b1236b4ed724eaceef2b79d4
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-12 14:25:20 -0300

    Create the session module and update the app module to use the session module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { SessionModule } from './Session/session.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,SessionModule],
   controllers: [],
   providers: [],
 })
