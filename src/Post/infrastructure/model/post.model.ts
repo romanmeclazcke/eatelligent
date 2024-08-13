65e0c818df25b4a6afca25d308700cd194f4ea08
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-13 08:51:55 -0300

    create post infrastructure

---
+++ src/Post/infrastructure/model/post.model.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from 'src/shared/infrastructure/db/db.sequelize.config';
+import User from 'src/user/infrastructure/models/user.models';
+
+class Post extends Model {
+    declare id: string;
+    declare userId: string;
+    declare description?: string;
+    declare image?: string;
+}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize, 
+    tableName: 'Post',
+    timestamps: true,
+  },
+);
+
+Post.belongsTo(User,{foreignKey:'userId',targetKey:'id',as:'author'})
+
+export default Post
