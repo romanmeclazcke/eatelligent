5db5962ee23434d9b9731b21d945884a8481feba
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-13 14:45:48 -0300

    add formatting to code

--- src/Post/application/post.use.cases.ts
+++ src/Post/application/post.use.cases.ts
@@ -5,10 +5,15 @@ import { Result } from 'src/shared/infrastructure/patternResult/result';
 import { postUpdateDto } from '../domain/dto/post.update.dto';
 import { UserEntity } from 'src/user/domian/user.entity';
 import { postCreateDto } from '../domain/dto/post.create.dto';
+import { CloudinaryService } from 'src/shared/infrastructure/cloudinary/cloudinary.service';
+import { profile } from 'console';
 
 @Injectable()
 export class postUsesCases {
-  constructor(private postRepository: postRepositorySequelize) {}
+  constructor(
+    private postRepository: postRepositorySequelize,
+    private cloudinary: CloudinaryService,
+  ) {}
 
   async getPostsFromUsersIFollow(
     userId: string,
@@ -30,24 +35,58 @@ export class postUsesCases {
     return Result.failure('Post not found', 404);
   }
 
-  async getPostByUser(userId: string): Promise<Result<postEntity[] | null>>{
+  async getPostByUser(userId: string): Promise<Result<postEntity[] | null>> {
     const posts = await this.postRepository.getPostByUser(userId);
     if (posts.length > 0) {
-      return Result.succes(posts, 200); 
+      return Result.succes(posts, 200);
     }
-    
+
     return Result.failure('Post not found', 404);
   }
 
-  async createPost(userId: string, postCreateDto: postCreateDto,file: Express.Multer.File): Promise<Result<postEntity | null>> {
-    return null
+  async createPost(
+    userId: string,
+    postCreateDto: postCreateDto,
+    file: Express.Multer.File,
+  ): Promise<Result<postEntity | null>> {
+    let postPictureUrl: string | null = null;
+
+    if (file) {
+      try {
+        const uploadResult = await this.cloudinary.uploadImage(file);
+        postPictureUrl = uploadResult.url;
+      } catch (uploadError) {
+        return Result.failure('Failed to upload image', 500);
+      }
+    }
+
+    const postToCreate: postCreateDto = {
+      ...postCreateDto,
+      image: postPictureUrl,
+    };
+
+    const post = await this.postRepository.createPost(userId, postToCreate);
+
+    if (post) {
+      return Result.succes(post, 200);
+    }
+
+    return Result.failure('Post not found', 404);
   }
 
-  async editPost(userId:string, id:string,postUpdateDto:postUpdateDto,file: Express.Multer.File):Promise<Result<UserEntity|null>>{
-    return null
+  async editPost(
+    userId: string,
+    id: string,
+    postUpdateDto: postUpdateDto,
+    file: Express.Multer.File,
+  ): Promise<Result<UserEntity | null>> {
+    return null;
   }
 
-  async deletePost(userId:string, id:string):Promise<Result<postEntity|null>>{
-    return null
+  async deletePost(
+    userId: string,
+    id: string,
+  ): Promise<Result<postEntity | null>> {
+    return null;
   }
 }
