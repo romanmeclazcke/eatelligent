ef7614a43218a14b6f8010e777836ed136795a72
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-13 11:20:17 -0300

    create favorite post model

---
+++ src/FavoritePost/infrastructure/model/favoritePost.model.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from 'sequelize';
+import Post from 'src/Post/infrastructure/model/post.model';
+import { sequelize } from 'src/shared/infrastructure/db/db.sequelize.config';
+import User from 'src/user/infrastructure/models/user.models';
+
+class FavoritePost extends Model {
+  
+}
+
+FavoritePost.init({
+  id: {
+    type: DataTypes.UUID, 
+    defaultValue: DataTypes.UUIDV4, 
+    allowNull: false,
+    primaryKey: true,
+  },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  postId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  addedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue:DataTypes.NOW
+  }
+},{
+    sequelize, 
+    tableName: 'FavoritePost',
+    timestamps: false,
+  }
+);
+
+FavoritePost.belongsTo(User,{foreignKey:'userId',targetKey:'id',as:'userFavoritePost'})
+FavoritePost.belongsTo(Post,{foreignKey:'postId',targetKey:'id',as:'favoritePost'})
+export default FavoritePost;
