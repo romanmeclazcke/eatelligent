1578b2d98a312833bc7a2135b3306b340cae7094
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-13 08:51:02 -0300

    add post module

---
+++ src/Post/post.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { CloudinaryModule } from 'src/shared/infrastructure/cloudinary/cloudinary.module';
+import { AuthService } from 'src/shared/infrastructure/auth/auth.service';
+import { postController } from './infrastructure/controller/post.controller';
+import { postUsesCases } from './application/post.use.cases';
+import { postRepositorySequelize } from './infrastructure/repository/post.repository.sequelize';
+
+
+@Module({
+  controllers: [postController],
+  providers:[postUsesCases,postRepositorySequelize],
+  exports:[],
+  imports:[CloudinaryModule]
+})
+export class UserModule {}
