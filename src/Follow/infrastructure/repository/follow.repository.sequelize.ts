004fc5d496240ac339d8f2c06776ce7a88032564
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-14 14:11:24 -0300

    create follow infrastructure

---
+++ src/Follow/infrastructure/repository/follow.repository.sequelize.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { followEntity } from "src/Follow/domain/follow.entity";
+import { followRepository } from "src/Follow/domain/follow.repository";
+import Follow from "../model/follow.model";
+
+@Injectable()
+export class followRepositorySequelize implements followRepository {
+    
+    
+    async followUser(followerId: string, followedId: string): Promise<followEntity | null> {
+        return await Follow.create({
+            
+            followerId:followerId,
+            followedId:followedId
+        })
+    }
+    
+    async unFollowUser(followerId: string, followedId: string): Promise<number | null> {
+        return Follow.destroy({
+            where:{
+                followerId:followerId,
+                followedId:followedId
+            }
+        })
+    }
+    
+    findIfFollow(followerId: string, followedId: string): Promise<followEntity | null> {
+        console.log(followerId,followedId)
+
+       return Follow.findOne({
+        attributes:['followedAt'],
+        where:{
+            followerId:followerId,
+            followedId:followedId
+        }
+       })
+    }
+}
