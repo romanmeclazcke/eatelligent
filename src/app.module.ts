5c8703f5e2cabb895b753fe138196a88f9e7b74d
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-14 13:08:38 -0300

    update app model to add like module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { SessionModule } from './Session/session.module';
+import { PostModule } from './Post/post.module';
+import { LikeModule } from './Like/like.module';
 @Module({
-  imports: [UserModule,SessionModule],
+  imports: [UserModule,SessionModule,PostModule,LikeModule],
   controllers: [],
   providers: [],
 })
