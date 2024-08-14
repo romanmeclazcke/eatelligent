9b615188aabc451e4ae38c662d8559cd1a41b914
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-14 14:12:14 -0300

    add followModule to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UserModule } from './user/user.module';
 import { SessionModule } from './Session/session.module';
 import { PostModule } from './Post/post.module';
 import { LikeModule } from './Like/like.module';
+import { FollowModule } from './Follow/follow.module';
 @Module({
-  imports: [UserModule,SessionModule,PostModule,LikeModule],
+  imports: [UserModule,SessionModule,PostModule,LikeModule,FollowModule],
   controllers: [],
   providers: [],
 })
