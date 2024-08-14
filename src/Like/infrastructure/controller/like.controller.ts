ea76fd2762ac09370aa97128848673037853e28e
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-14 13:08:05 -0300

    create like infrastructure

---
+++ src/Like/infrastructure/controller/like.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Delete, Param, ParseUUIDPipe, Post, Req, Res } from "@nestjs/common";
+import { likeUseCases } from "src/Like/application/like.use.cases";
+import {Request,Response} from "express"
+
+@Controller('like')
+export class likeController{
+
+    constructor(private likeUseCases: likeUseCases){}
+
+    @Post('/give-like/:userId/:postId')
+    async giveLike(
+        @Param('userId', ParseUUIDPipe) userId: string,
+        @Param('userId', ParseUUIDPipe) postId: string,
+        @Req() request: Request,
+        @Res() res: Response,
+      ) {
+        const result = await this.likeUseCases.giveLike(userId,postId);
+        if (result.isSucces) {
+          res.status(result.statusCode).json({ message: result.value, details: true });
+        } else {
+          res.status(result.statusCode).json({ message: result.error, details: false });
+        }
+      }
+
+
+    
+
+    @Delete('/dislike-post/:userId/:postId')
+    async dislikePost(
+        @Param('userId', ParseUUIDPipe) userId: string,
+        @Param('userId', ParseUUIDPipe) postId: string,
+        @Req() request: Request,
+        @Res() res: Response,
+      ) {
+        const result = await this.likeUseCases.giveLike(userId,postId);
+        if (result.isSucces) {
+          res.status(result.statusCode).json({ message: result.value, details: true });
+        } else {
+          res.status(result.statusCode).json({ message: result.error, details: false });
+        }
+      }
+}
