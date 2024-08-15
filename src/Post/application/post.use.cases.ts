3992bbdf4bcfb1d34d1ff9f7046afc257d5d225e
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-15 09:44:30 -0300

    add verification if user and post exist

--- src/Post/application/post.use.cases.ts
+++ src/Post/application/post.use.cases.ts
@@ -3,21 +3,26 @@ import { postRepositorySequelize } from '../infrastructure/repository/post.repos
 import { postEntity } from '../domain/post.entity';
 import { Result } from 'src/shared/infrastructure/patternResult/result';
 import { postUpdateDto } from '../domain/dto/post.update.dto';
-import { UserEntity } from 'src/user/domian/user.entity';
 import { postCreateDto } from '../domain/dto/post.create.dto';
 import { CloudinaryService } from 'src/shared/infrastructure/cloudinary/cloudinary.service';
-import { profile } from 'console';
+import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
 
 @Injectable()
 export class postUsesCases {
   constructor(
     private postRepository: postRepositorySequelize,
     private cloudinary: CloudinaryService,
+    private userRepository: userRepositorySequelize,
   ) {}
 
   async getPostsFromUsersIFollow(
     userId: string,
   ): Promise<Result<postEntity[]>> {
+    const user = await this.userRepository.getUserById(userId);
+
+    if (!user) {
+      return Result.failure('User not found', 404);
+    }
     const posts = await this.postRepository.getPostsFromUsersIFollow(userId);
 
     if (posts) {
@@ -36,6 +41,12 @@ export class postUsesCases {
   }
 
   async getPostByUser(userId: string): Promise<Result<postEntity[] | null>> {
+    const user = await this.userRepository.getUserById(userId);
+
+    if (!user) {
+      return Result.failure('User not found', 404);
+    }
+
     const posts = await this.postRepository.getPostByUser(userId);
     if (posts.length > 0) {
       return Result.succes(posts, 200);
@@ -49,6 +60,12 @@ export class postUsesCases {
     postCreateDto: postCreateDto,
     file: Express.Multer.File,
   ): Promise<Result<postEntity | null>> {
+    const user = await this.userRepository.getUserById(userId);
+
+    if (!user) {
+      return Result.failure('User not found', 404);
+    }
+
     let postPictureUrl: string | null = null;
 
     if (file) {
@@ -68,10 +85,10 @@ export class postUsesCases {
     const post = await this.postRepository.createPost(userId, postToCreate);
 
     if (post) {
-      return Result.succes(post, 200);
+      return Result.succes(post, 201);
     }
 
-    return Result.failure('Post not found', 404);
+    return Result.failure('Internal srever error', 500);
   }
 
   async editPost(
@@ -79,14 +96,50 @@ export class postUsesCases {
     id: string,
     postUpdateDto: postUpdateDto,
     file: Express.Multer.File,
-  ): Promise<Result<UserEntity | null>> {
-    return null;
+  ): Promise<Result<postEntity | null>> {
+    const user = await this.userRepository.getUserById(userId);
+
+    if (!user) {
+      return Result.failure('User not found', 404);
+    }
+    let postPictureUrl: string | null = null;
+
+    if (file) {
+      try {
+        const uploadResult = await this.cloudinary.uploadImage(file);
+        postPictureUrl = uploadResult.url;
+      } catch (uploadError) {
+        return Result.failure('Failed to upload image', 500);
+      }
+    }
+
+    const postToUpdated: postUpdateDto = {
+      ...postUpdateDto,
+      image: postPictureUrl,
+    };
+
+    const postUpdated = await this.postRepository.editPost(
+      userId,
+      id,
+      postToUpdated,
+    );
+
+    if (postUpdated) {
+      return Result.succes(postUpdated, 200);
+    }
+
+    return Result.failure('Internal server error', 500);
   }
 
   async deletePost(
     userId: string,
     id: string,
-  ): Promise<Result<postEntity | null>> {
-    return null;
+  ): Promise<Result<string | null>> {
+    const postDeleted = await this.postRepository.deletePost(userId, id);
+  
+    if (postDeleted) {
+      return Result.succes("Post succesfully deleted", 200);
+    }
+    return Result.failure('Internal server error', 500);
   }
 }
