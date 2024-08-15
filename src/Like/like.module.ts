3746ef55088a23a6f158cae5c8509c548693f9e1
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-15 12:51:50 -0300

    resolved error on dependencies of each module

--- src/Like/like.module.ts
+++ src/Like/like.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { likeController } from './infrastructure/controller/like.controller';
 import { likeUseCases } from './application/like.use.cases';
 import { likeRepositorySequelize } from './infrastructure/repository/like.repository.sequelize';
+import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
+import { postRepositorySequelize } from 'src/Post/infrastructure/repository/post.repository.sequelize';
 
 
 @Module({
   controllers: [likeController],
-  providers:[likeUseCases,likeRepositorySequelize],
+  providers:[likeUseCases,likeRepositorySequelize,userRepositorySequelize, postRepositorySequelize],
   exports:[],
   imports:[]
 })
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { SessionModule } from './Session/session.module';
 import { PostModule } from './Post/post.module';
 import { LikeModule } from './Like/like.module';
 import { FollowModule } from './Follow/follow.module';
+import { CommentModule } from './Comment/comment.module';
 @Module({
-  imports: [UserModule,SessionModule,PostModule,LikeModule,FollowModule],
+  imports: [UserModule,SessionModule,PostModule,LikeModule,FollowModule,CommentModule],
   controllers: [],
   providers: [],
 })
