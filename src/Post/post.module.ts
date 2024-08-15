1502d8636e3b2d1274600b1db9b9bff920cc0979
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-15 12:48:26 -0300

    implement function get post with comments and count of likes

--- src/Post/post.module.ts
+++ src/Post/post.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryModule } from 'src/shared/infrastructure/cloudinary/cloudinary.module';
-import { AuthService } from 'src/shared/infrastructure/auth/auth.service';
 import { postController } from './infrastructure/controller/post.controller';
 import { postUsesCases } from './application/post.use.cases';
 import { postRepositorySequelize } from './infrastructure/repository/post.repository.sequelize';
+import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
 
 
 @Module({
   controllers: [postController],
-  providers:[postUsesCases,postRepositorySequelize],
+  providers:[postUsesCases,postRepositorySequelize,userRepositorySequelize],
   exports:[],
   imports:[CloudinaryModule]
 })
