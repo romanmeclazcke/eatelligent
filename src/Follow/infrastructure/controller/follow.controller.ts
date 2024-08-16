3dd3b3286e873b90bbb079d654a9364549030b50
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-16 09:18:15 -0300

    create follow controller

--- src/Follow/infrastructure/controller/follow.controller.ts
+++ src/Follow/infrastructure/controller/follow.controller.ts
@@ -1,44 +1,57 @@
-import { Controller, Delete, Param, ParseUUIDPipe, Post, Req, Res } from "@nestjs/common";
-import { followUseCases } from "src/Follow/application/follow.use.cases";
-import { Request,Response } from "express";
+import {
+  Controller,
+  Delete,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { followUseCases } from 'src/Follow/application/follow.use.cases';
+import { Request, Response } from 'express';
 
 @Controller('follow')
-export class followController{
+export class followController {
+  constructor(private followUseCases: followUseCases) {}
 
-    constructor(private followUseCases: followUseCases){}
-
-
-    @Post('/:followerId/:followedId')
-    async followUser(
-        @Param('followerId', ParseUUIDPipe) followerId: string,
-        @Param('followedId', ParseUUIDPipe) followedId: string,
-        @Req() request: Request,
-        @Res() res: Response,
-    ){
-        
-    const result = await this.followUseCases.followUser(followerId,followedId);
+  @Post('/:followerId/:followedId')
+  async followUser(
+    @Param('followerId', ParseUUIDPipe) followerId: string,
+    @Param('followedId', ParseUUIDPipe) followedId: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const result = await this.followUseCases.followUser(followerId, followedId);
     if (result.isSucces) {
-      res.status(result.statusCode).json({ message: result.value, details: true });
+      res
+        .status(result.statusCode)
+        .json({ message: result.value, details: true });
     } else {
-      res.status(result.statusCode).json({ message: result.error, details: false });
+      res
+        .status(result.statusCode)
+        .json({ message: result.error, details: false });
     }
-    }
-    
+  }
 
-    @Delete('/:followerId/:followedId')
-    async unFollowUser(
-        @Param('followerId', ParseUUIDPipe) followerId: string,
-        @Param('followedId', ParseUUIDPipe) followedId: string,
-        @Req() request: Request,
-        @Res() res: Response,
-    ){
-        
-    const result = await this.followUseCases.unFollowUser(followerId,followedId);
+  @Delete('/:followerId/:followedId')
+  async unFollowUser(
+    @Param('followerId', ParseUUIDPipe) followerId: string,
+    @Param('followedId', ParseUUIDPipe) followedId: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const result = await this.followUseCases.unFollowUser(
+      followerId,
+      followedId,
+    );
     if (result.isSucces) {
-      res.status(result.statusCode).json({ message: result.value, details: true });
+      res
+        .status(result.statusCode)
+        .json({ message: result.value, details: true });
     } else {
-      res.status(result.statusCode).json({ message: result.error, details: false });
+      res
+        .status(result.statusCode)
+        .json({ message: result.error, details: false });
     }
-    }
-
+  }
 }
