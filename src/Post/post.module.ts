ce2f3722cb56e77a54bba989400e871f805e6053
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-16 18:18:23 -0300

    add bad words service to post.module

--- src/Post/post.module.ts
+++ src/Post/post.module.ts
@@ -4,11 +4,12 @@ import { postController } from './infrastructure/controller/post.controller';
 import { postUsesCases } from './application/post.use.cases';
 import { postRepositorySequelize } from './infrastructure/repository/post.repository.sequelize';
 import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
+import { badWordsService } from 'src/shared/infrastructure/IAtext/bad.word.service';
 
 
 @Module({
   controllers: [postController],
-  providers:[postUsesCases,postRepositorySequelize,userRepositorySequelize],
+  providers:[postUsesCases,postRepositorySequelize,userRepositorySequelize, badWordsService],
   exports:[],
   imports:[CloudinaryModule]
 })
