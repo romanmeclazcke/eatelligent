a3981e5060b88fda6c741c41f0a20ea82214c71e
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-18 11:41:50 -0300

    defined meal module and add providers

--- src/Meal/meal.module.ts
+++ src/Meal/meal.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { mealController } from './infrastructure/controller/meal.controller';
 import { mealUseCases } from './application/meal.use.cases';
 import { mealRepositorySequelize } from './infrastructure/repository/meal.repository.sequelize';
+import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
 
 
 @Module({
   controllers: [mealController],
-  providers:[mealUseCases,mealRepositorySequelize],
+  providers:[mealUseCases,mealRepositorySequelize,userRepositorySequelize],
   exports:[],
   imports:[]
 })
