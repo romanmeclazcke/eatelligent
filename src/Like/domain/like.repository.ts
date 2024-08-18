8c799d001a070ae0ef1770883462c7e4cca8bdbe
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-18 11:42:25 -0300

    add method findLike

--- src/Like/domain/like.repository.ts
+++ src/Like/domain/like.repository.ts
@@ -3,4 +3,5 @@ import { likeEntity } from "./like.entity";
 export interface likeRepository{
     likePost(userId:string,postId:string):Promise<likeEntity>
     dislikePost(userId:string,postId:string):Promise<number>
+    findLike(userId:string, postId:string):Promise<likeEntity|null>
 }
