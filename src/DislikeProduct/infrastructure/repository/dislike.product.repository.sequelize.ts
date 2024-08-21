ec8771793a83f84460f31ec8844abfebe79210f2
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-21 10:02:09 -0300

    create dislike product repository

---
+++ src/DislikeProduct/infrastructure/repository/dislike.product.repository.sequelize.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { dislikeProductEntity } from "src/DislikeProduct/domain/dislike.product.entity";
+import { dislikeProductRepository } from "src/DislikeProduct/domain/dislike.product.repository";
+import DislikeProduct from "../model/dislike.product.model";
+
+@Injectable()
+export class dislikeProductRepositorySequelize implements dislikeProductRepository{
+
+    async getDislikeProduct(userId: string): Promise<dislikeProductEntity[]> {
+        return await DislikeProduct.findAll({
+            where:{userId:userId}
+        })
+    }
+    async addDislikeProduct(userId: string, productId: String): Promise<dislikeProductEntity> {
+       return await DislikeProduct.create({
+        userId:userId,
+        productId:productId
+       })
+    }
+    
+    async deleteDislikeProduct(userId:string,productId:string): Promise<number> {
+        return await DislikeProduct.destroy({where:{
+            userId:userId,
+            productId:productId
+        }})
+    }
+}
