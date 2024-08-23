0a0db2d533af5f21b1cc15995aee14785cc2fb83
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-23 10:10:08 -0300

    add method to get meal by user testes

--- src/Meal/infrastructure/repository/meal.repository.sequelize.ts
+++ src/Meal/infrastructure/repository/meal.repository.sequelize.ts
@@ -4,21 +4,55 @@ import { mealUpdateDto } from "src/Meal/domain/dto/meal.update.dto";
 import { mealEntity } from "src/Meal/domain/meal.entity";
 import { mealRepository } from "src/Meal/domain/meal.repository";
 import Meal from "../model/meal.model";
+import Product from "src/Product/infrastructure/model/product.model";
+import DislikeProduct from "src/DislikeProduct/infrastructure/model/dislike.product.model";
+import { Op } from 'sequelize';
 
 @Injectable()
 export class mealRepositorySequelize implements mealRepository{
 
-    async getMealsByTastes(userId: string): Promise<mealEntity | null> {
-        throw new Error("Method not implemented.");
+    async getMealsByTastes(userId: string): Promise<mealEntity[] | null> {
+        const dislikeProducts = await DislikeProduct.findAll({
+            attributes: ['productId'],  
+            where: {
+                userId: userId
+            }
+        });
+        
+
+        if(dislikeProducts.length==0){//no tiene productos que no le gusten entonces retorno todas las comidas
+            return await Meal.findAll();
+        }
+
+
+        const dislikedProductIds = dislikeProducts.map(dislike => dislike.productId);
+
+        return await Meal.findAll({
+            include: [{
+                model: Product,
+                attributes: [], // No necesitas los atributos del modelo `Product` aquí
+                through: { attributes: [] },
+                where: {
+                    id: {
+                        [Op.notIn]: dislikedProductIds //revisar error (si el la comida tiene un producto que al usario le gusta la muestra igual aunque tenga otros que no)
+                    }
+                }
+            }]
+        });
+        //obtener todas las comidas que no incluyen productos que al usuario no le gustan.
     }
-    async getMealsById(mealId: string): Promise<mealEntity | null> {
+    async getMealById(mealId: string): Promise<mealEntity | null> {
         return await Meal.findOne({
             where:{
                 id:mealId
-            }
+            },
+            include: [{
+                model: Product,
+                through: { attributes: [] }, // Opcional, para no traer los atributos de la tabla intermedia 
+            }]
         })
     }
-    async createMeals(mealCreateDto: mealCreateDto): Promise<mealEntity | null> {
+    async createMeal(mealCreateDto: mealCreateDto): Promise<Meal | null> {
         return await Meal.create({
             ...mealCreateDto
         })
@@ -34,7 +68,7 @@ export class mealRepositorySequelize implements mealRepository{
             return null
         }
 
-        return this.getMealsById(mealId)
+        return this.getMealById(mealId)
     }
     async deleteMeal(mealId: string): Promise<number | null> {
         return await Meal.destroy({
