8cee0a977f64ba41646ffc4a6cc8f3b7f8644054
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-08-23 10:17:08 -0300

    add method to find dislike product

--- src/DislikeProduct/infrastructure/repository/dislike.product.repository.sequelize.ts
+++ src/DislikeProduct/infrastructure/repository/dislike.product.repository.sequelize.ts
@@ -5,12 +5,22 @@ import DislikeProduct from "../model/dislike.product.model";
 
 @Injectable()
 export class dislikeProductRepositorySequelize implements dislikeProductRepository{
-
+    
     async getDislikeProduct(userId: string): Promise<dislikeProductEntity[]> {
         return await DislikeProduct.findAll({
             where:{userId:userId}
         })
     }
+
+    async findDislikedProduct(userId: string, productId: string): Promise<dislikeProductEntity | null> {
+        return await DislikeProduct.findOne({
+            where:{
+                userId:userId,  
+                productId:productId
+            }
+        })
+    }
+    
     async addDislikeProduct(userId: string, productId: String): Promise<dislikeProductEntity> {
        return await DislikeProduct.create({
         userId:userId,
