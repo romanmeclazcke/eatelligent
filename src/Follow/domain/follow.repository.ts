84c5b43c3007e14c9f33d70b741ce6997ee9c589
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-09-02 09:44:14 -0300

    declared method in  inteface follow repository

--- src/Follow/domain/follow.repository.ts
+++ src/Follow/domain/follow.repository.ts
@@ -5,4 +5,6 @@ export interface followRepository{
     followUser(followerId:string, followedId:string):Promise<followEntity|null>
     unFollowUser(followerId:string, followedId:string):Promise<number|null>
     findIfFollow(followerId:string, followedId:string):Promise<followEntity|null>
+    getListOfFollowers(userId:string):Promise<followEntity[]|null>
+    getListOfFolloweds(userId:string):Promise<followEntity[]|null>
 }
