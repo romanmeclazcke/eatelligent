63605487e47f7f3dfd12da6d442fddbc774f7196
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-09-05 13:36:29 -0300

    create favorite meal model

---
+++ src/FavoriteMeal/infrastructure/model/favorite.meal.model.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from 'sequelize';
+import Meal from 'src/Meal/infrastructure/model/meal.model';
+import { sequelize } from 'src/shared/infrastructure/db/db.sequelize.config';
+import User from 'src/user/infrastructure/models/user.models';
+
+class FavoriteMeal extends Model {
+  declare id: string;
+  declare userId: string;
+  declare mealId: string;
+}
+
+FavoriteMeal.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,  // Usa 'id' como clave primaria
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    mealId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'FavoriteMeal',
+    timestamps: true, // Esto manejará `createdAt` y `updatedAt`
+  },
+);
+
+// Asociaciones
+FavoriteMeal.belongsTo(User, { foreignKey: 'userId', targetKey: 'id', as: 'userFavorites' });
+FavoriteMeal.belongsTo(Meal, { foreignKey: 'mealId', targetKey: 'id', as: 'favoriteMeal' });
+
+export default FavoriteMeal;
