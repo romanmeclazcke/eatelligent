04fb94f9cfdb18ae2c6cb0873c1d1801ac584814
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-09-06 12:29:14 -0300

    remove unused module

--- src/Session/domain/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString, isString } from "class-validator";
-
-export class LoginDto{
-    @IsEmail() //puedo agregare propiedades para dar una respuesta especifica
-    readonly  email: string;
-
-    @IsString()
-    readonly  password: string;
-}
