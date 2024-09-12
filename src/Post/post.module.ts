8a4285672448557f66a55465248de03cf753111c
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-09-12 13:26:33 -0300

    add providers of post module

--- src/Post/post.module.ts
+++ src/Post/post.module.ts
@@ -5,11 +5,12 @@ import { postUsesCases } from './application/post.use.cases';
 import { postRepositorySequelize } from './infrastructure/repository/post.repository.sequelize';
 import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
 import { badWordsService } from 'src/shared/infrastructure/IAtext/bad.word.service';
+import { SightEngineServices } from 'src/shared/infrastructure/IAimage/sight.engine.service';
 
 
 @Module({
   controllers: [postController],
-  providers:[postUsesCases,postRepositorySequelize,userRepositorySequelize, badWordsService],
+  providers:[postUsesCases,postRepositorySequelize,userRepositorySequelize, badWordsService,SightEngineServices ],
   exports:[],
   imports:[CloudinaryModule]
 })
