6b59e2cd9eba1c8eaa0133a798fd97d79d17dda5
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-09-20 13:55:11 -0300

    update method get post by id user

--- src/user/infrastructure/repository/user.repository.sequelize.ts
+++ src/user/infrastructure/repository/user.repository.sequelize.ts
@@ -4,73 +4,118 @@ import { userRepository } from 'src/user/domian/user.repository';
 import User from '../models/user.models';
 import { CreateUserDto } from 'src/user/domian/dto/create.user.dto';
 import { UpdateUserDto } from 'src/user/domian/dto/user.update';
+import Follow from 'src/Follow/infrastructure/model/follow.model';
+import { Op, Sequelize } from 'sequelize';
 
 @Injectable()
 export class userRepositorySequelize implements userRepository {
-    
-    
-    async  getUsers(): Promise<UserEntity[] | null> {
-        const user =await User.findAll()
-        return user
+  async getUsers(): Promise<UserEntity[] | null> {
+    return await User.findAll();
+  }
+
+  async getUserById(id: string): Promise<UserEntity | null> {
+    return await User.findByPk(id, {
+      attributes: [
+        'name',
+        'lastName',
+        'userName',
+        'profilePicture',
+        'validateEmail',
+      ],
+    });
+  }
+  //add method findIfUserExist(); whithout attributes
+
+  async getUserByEmail(email: string) {
+    return await User.findOne({
+      attributes: ['id'],
+      where: { email: email },
+    });
+  }
+
+  async getUserByUserName(userName: string) {
+    return await User.findOne({
+      attributes: ['id'],
+      where: {
+        userName: userName,
+      },
+    });
+  }
+
+  async createUser(user: CreateUserDto): Promise<UserEntity | null> {
+    try {
+      const createdUser = await User.create({
+        ...user,
+        birthdate: new Date(user.birthdate), // Conversión de string a Date
+        validateEmail: false,
+      });
+      return createdUser;
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return null;
     }
+  }
+
+  async updateUserInformation(
+    user: UpdateUserDto,
+    id: string,
+  ): Promise<UserEntity | null> {
+    const userUpdated = await User.update(user, {
+      where: {
+        id: id,
+      },
+    });
+    return this.getUserById(id);
+  }
+
+  async updateProfilePicture(
+    file: string,
+    id: string,
+  ): Promise<UserEntity | null> {
+    const userUpdated = await User.update(
+      { profilePicture: file },
+      {
+        where: { id: id },
+      },
+    );
+    return this.getUserById(id);
+  }
+
+  async getRecomendationUsers(userId: string): Promise<UserEntity[] | null> {
+   //traigo todos los usuarios que sigo
+    const followedUsers = await Follow.findAll({
+      where: {
+        followerId: userId,
+      },
+      attributes: ['followedId'],
+    });
     
-    async getUserById(id:string): Promise<UserEntity | null> {
-        return await User.findByPk(id,{
-            attributes:['name','lastName','userName','profilePicture','validateEmail']
-        })
-    }
-    
-    async getUserByEmail(email: string) {
-        return await User.findOne({
-            attributes: ['id'] ,
-            where:{email:email},
-            
-        })
-    }
-    
-    async getUserByUserName(userName: string) {
-        return await User.findOne({
-            attributes:['id'],
-            where:{
-                userName:userName
+    //extraigo el id de los resultados
+    const followedIds = followedUsers.map((f) => f.followedId);
+    console.log(followedIds);
+   
+
+    const seguiresSegudiosPorMisSeguidos =await Follow.findAll({ //refactorizar esto
+        where:{
+            followerId:{
+                [Op.in]:followedIds
             }
-        })
-    }
-    
-    async createUser(user: CreateUserDto): Promise<UserEntity | null> {
-        try {
-            const createdUser = await User.create({
-                ...user, 
-                birthdate: new Date(user.birthdate), // Conversión de string a Date
-                validateEmail:false
-            });
-            return createdUser;
-        } catch (error) {
-            console.error('Error creating user:', error);
-            return null;
         }
-    }
-    
-    async updateUserInformation(user: UpdateUserDto,id:string): Promise<UserEntity | null> {
-        const userUpdated= await User.update(user,{
-                where:{
-                    id:id
-                }
-            }
-        )
-        return this.getUserById(id)
-    }
+    })
+    const test = seguiresSegudiosPorMisSeguidos.map((f) => f.followedId);
+    console.log("Estos son los usuarios que tus usuarios seguidos siguen",test);
 
-    async updateProfilePicture (file: string, id: string): Promise<UserEntity | null> {
-        const userUpdated = await User.update(
-            {profilePicture:file},
-            {
-                where:{id:id}
-            })
-        return this.getUserById(id)
-    }
-    
-    
+    const result = await User.findAll({ //traigo todos los usuarios que su id esta en la lista de usuarios que mis seguidos siguen
+        where: {
+          id: {
+            [Op.in]: test
+          }
+        },
+        attributes: ['id', 'username', 'profilePicture'], // Atributos del usuario
+      });
+      
+    console.log(result);
+    return result;
+}
 
-    
 }
