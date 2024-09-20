c9bb6af36a4aa942d61a1402b2283d33bdf1a26d
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-09-20 13:58:05 -0300

    created  profile entity

---
+++ src/ShowProfile/domain/profile.entity.ts
@@ -0,0 +1,10 @@
+import { followStatsEntity } from "src/Follow/domain/follow.stats.entity"
+import { postEntity } from "src/Post/domain/post.entity"
+import { UserEntity } from "src/user/domian/user.entity"
+
+export interface profileEntity{
+    id:string
+    user:UserEntity
+    followStats:followStatsEntity
+    posts: postEntity[]
+}
