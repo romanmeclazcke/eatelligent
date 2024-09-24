1563a0dd58ee65fe55e3d98cad1ea251a71e114f
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-09-24 10:55:03 -0300

    added reflactor to auth guard and implemented logic to get metadata of public method

--- src/shared/infrastructure/guards/auth.guard.jwt.ts
+++ src/shared/infrastructure/guards/auth.guard.jwt.ts
@@ -4,25 +4,33 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { tokenInterface } from 'src/Auth/domain/token.interface';
 import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
 
 @Injectable()
 export class AuthGuardJwt implements CanActivate {
-  constructor(private jwtService: JwtService, private userRepository: userRepositorySequelize) {}
+  constructor(
+    private jwtService: JwtService,
+    private userRepository: userRepositorySequelize,
+    private readonly reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.get<boolean>('isPublic',context.getHandler());//si e
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromHeader(request);
-    
+
     if (!token) {
       console.log('No token found');
       throw new UnauthorizedException('No token found');
     }
-    console.log('Token:', token);
-    
+
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET,
@@ -32,10 +40,9 @@ export class AuthGuardJwt implements CanActivate {
 
       const user = await this.userRepository.getUserById(id); //obtengo el estado de la cuenta del user
 
-      if (!user.validateEmail) { //si no valido el email respondo con Unauthorized aunque nunca va a pasar, dado que para acceeder a un token debes validar tu email
-        console.log('Email not validated');
+      //si no valido el email respondo con Unauthorized aunque nunca va a pasar, dado que para acceeder a un token debes validar tu email
+      if (!user.validateEmail)
         throw new UnauthorizedException('Email not validated');
-      }
 
     } catch (error) {
       console.error('Token verification failed:', error.message);
