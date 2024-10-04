70e72946803e7fbbd37611c4bde46a01e2a43d82
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-04 13:09:05 -0300

    updated meal module

--- src/Meal/meal.module.ts
+++ src/Meal/meal.module.ts
@@ -4,12 +4,13 @@ import { mealUseCases } from './application/meal.use.cases';
 import { mealRepositorySequelize } from './infrastructure/repository/meal.repository.sequelize';
 import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
 import { CloudinaryService } from 'src/Shared/infrastructure/cloudinary/cloudinary.service';
+import { CloudinaryModule } from 'src/Shared/infrastructure/cloudinary/cloudinary.module';
 
 
 @Module({
   controllers: [mealController],
-  providers:[mealUseCases,mealRepositorySequelize,userRepositorySequelize,CloudinaryService],
+  providers:[mealUseCases,mealRepositorySequelize,userRepositorySequelize],
   exports:[],
-  imports:[]
+  imports:[CloudinaryModule]
 })
 export class MealModule {}
