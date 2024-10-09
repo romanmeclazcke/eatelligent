77c9b24a17e155db22de23cf2d72f9992cb45982
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-09 15:49:30 -0300

    created entdpoint to change password

--- src/Auth/infrastructure/controller/auth.controller.ts
+++ src/Auth/infrastructure/controller/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { authUseCases } from 'src/Auth/application/auth.use.cases';
+import { changePasswordDto } from 'src/Auth/domain/dto/change.password.dto';
 import { loginDto } from 'src/Auth/domain/dto/login.dto';
 import { Public } from 'src/Shared/infrastructure/decorators/is.public';
 
@@ -33,6 +34,42 @@ export class authController {
   ) {
     console.log("entre")
     const result = await this.authUseCases.validateAccount(token);
+    result.isSucces
+      ? res
+          .status(result.statusCode)
+          .json({ message: result.value, details: true })
+      : res
+          .status(result.statusCode)
+          .json({ message: result.error, details: false });
+  }
+
+
+  @Get('/change-password/:userId')
+  async changePassword(
+    @Query('userId') userId: string,
+    @Body() changePasswordDto:changePasswordDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    console.log("entre")
+    const result = await this.authUseCases.changePassword(userId,changePasswordDto);
+    result.isSucces
+      ? res
+          .status(result.statusCode)
+          .json({ message: result.value, details: true })
+      : res
+          .status(result.statusCode)
+          .json({ message: result.error, details: false });
+  }
+
+  @Get('/reset-password/:userId')
+  async sendEmailResetPassword(
+    @Query('userId') userId: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    console.log("entre")
+    const result = await this.authUseCases.sendEmailResetPassword(userId);
     result.isSucces
       ? res
           .status(result.statusCode)
