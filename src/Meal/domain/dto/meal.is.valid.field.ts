6c7dff771843aea544df66f153097b551a529f28
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-14 13:27:58 -0300

    create meals attribute validator

---
+++ src/Meal/domain/dto/meal.is.valid.field.ts
@@ -0,0 +1,24 @@
+import { ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+import { mealEntity } from "../meal.entity";
+
+const validFields: (keyof mealEntity)[] = [
+  'id',
+  'name',
+  'mealPicture',
+  'recipe',
+  'calories',
+  'protein',
+  'carbohydrates',
+  'glutenFree',
+];
+
+@ValidatorConstraint({ name: 'IsValidFieldMeals', async: false })
+export class IsValidFieldMeals implements ValidatorConstraintInterface {
+  validate(value: any): boolean {
+    return validFields.includes(value);
+  }
+
+  defaultMessage(): string {
+    return `El campo sortBy debe ser uno de los siguientes: ${validFields.join(', ')}`;
+  }
+}
