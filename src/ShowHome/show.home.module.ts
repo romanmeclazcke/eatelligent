f89c764d3aa2556df2096a51a0209c40183b50fb
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-18 10:59:10 -0300

    update controller of show home module

--- src/ShowHome/show.home.module.ts
+++ src/ShowHome/show.home.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { userRepositorySequelize } from 'src/user/infrastructure/repository/user.repository.sequelize';
 import { postRepositorySequelize } from 'src/Post/infrastructure/repository/post.repository.sequelize';
 import { followRepositorySequelize } from 'src/Follow/infrastructure/repository/follow.repository.sequelize';
+import { showHomeController } from './infrastructure/controller/show.home.controller';
 
 
 @Module({
-  controllers: [],
+  controllers: [showHomeController],
   providers:[userRepositorySequelize,postRepositorySequelize, followRepositorySequelize],
   exports:[],
   imports:[]
