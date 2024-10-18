67b2025ec78cc3e1089d50039039935e25325bfa
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-18 10:59:45 -0300

    create show home controller

--- src/ShowHome/infrastructure/controller/show.home.controller.ts
+++ src/ShowHome/infrastructure/controller/show.home.controller.ts
@@ -1,6 +1,25 @@
-import { Controller } from "@nestjs/common";
-
+import { Controller, Get, Param, ParseUUIDPipe, Req, Res } from "@nestjs/common";
+import { showHomeUseCases } from "src/ShowHome/application/show.home.use.cases";
+import { Request,Response } from "express";
 @Controller('home')
-export class showHomeController{
+export class showHomeController {
+
+    constructor(private showHomeUseCases: showHomeUseCases) { }
+
+    @Get('/:userId')
+    async showHome(
+        @Param('userId', ParseUUIDPipe) userId: string,
+        @Req() req: Request,
+        @Res() res: Response
+    ) {
+        const result = await this.showHomeUseCases.showHome(userId);
+        result.isSucces
+            ? res
+                .status(result.statusCode)
+                .json({ message: result.value, details: true })
+            : res
+                .status(result.statusCode)
+                .json({ message: result.error, details: false });
+    }
 
 }
